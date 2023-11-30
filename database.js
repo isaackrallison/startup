b0db1f2e03e11296cd32905a182f9a141cc0b6d3
Isaac Rallison <isaacrallison@gmail.com>
2023-11-30 15:03:47 -0700

    updating database.js

--- database.js
+++ database.js
@@ -6,15 +6,8 @@ const client = new MongoClient(url);
 const db = client.db('startup');
 const suggestionCollection = db.collection('suggestion');
 
-// async function addSuggestion(suggestion) {
-//     const result = await suggestionCollection.insertOne(suggestion);
-//     return result;
-// }
-
 async function addSuggestion(suggestion) {
-  // Set the initial count to 0 when adding a new suggestion
   suggestion.count = 0;
-
   const result = await suggestionCollection.insertOne(suggestion);
   return result;
 }
