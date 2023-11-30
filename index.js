aae7e3cee7c05012f6ea513abe4b7203ae1a0c04
Isaac Rallison <isaacrallison@gmail.com>
2023-11-29 22:13:30 -0700

    changes to add database functionality

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const DB = require('./database.js');
 
 // The service port. In production the frontend code is statically hosted by the service on the same port.
-const port = process.argv.length > 2 ? process.argv[2] : 4000;
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
@@ -15,13 +16,15 @@ const apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
 // Getsuggestions
-apiRouter.get('/suggestions', (_req, res) => {
+apiRouter.get('/suggestions', async (_req, res) => {
+  const suggestions = await DB.getSuggestions();
   res.send(suggestions);
 });
 
 // SubmitSuggestions
-apiRouter.post('/suggestions', (req, res) => {
-  suggestions = updatesuggestions(req.body, suggestions);
+apiRouter.post('/suggestions', async (req, res) => {
+  DB.addSuggestion(req.body);
+  const suggestions  = await DB.getSuggestions();
   res.send(suggestions);
 });
 
